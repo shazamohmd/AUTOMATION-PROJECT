92aa4ebaa44b1479ea9d75c468a15d53368063e9
shaza <shaza@gmail.com>
2025-05-03 22:43:52 +0300

    organize

--- terraform/Module/task/variables.tf
+++ terraform/Module/task/variables.tf
@@ -1,8 +1,3 @@
-variable "aws_region" {
-  description = "The AWS region to deploy resources in"
-  type        = string
-}
-
 variable "project_name" {
   description = "Name of the project"
   type        = string
