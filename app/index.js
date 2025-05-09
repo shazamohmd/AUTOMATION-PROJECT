38a23f6ff39a751ecf49732534dcb7c78e6794b9
shaza <shaza@gmail.com>
2025-05-10 00:34:36 +0300

    default

--- app/index.js
+++ app/index.js
@@ -1,11 +1,11 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
 
-const server = createServer((req, res) => {
-  res.statusCode = 200
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
   const msg = 'Hello Node!\n'
-  res.end(msg)
-})
+  res.end(msg);
+});
 
 server.listen(port, () => {
   console.log(`Server running on http://localhost:${port}/`)
